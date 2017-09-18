d630a3944bdd7f7bda1cecfb2a3ac8c42d8ce498
Abhijeet <vaikar@gmail.com>
2017-09-18 20:36:59 +0530

    basic introduction to python in the helloworld.py exercise. Next is variables

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,27 @@
 print("Hello World")
+print(1 + 2)
+print(7 + 6)
+print()
+print("The End")
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + "world")
+greeting = "Hello"
+name = "Bruce"
+# this is a comment
+print(greeting + " " + name)
+yourName = input("Please enter your name")
+print(greeting + ' ' + yourName)
+splitString = "This string has been\nsplit over\n several \nlines"
+print(splitString)
+tabbedString = "1\t2\t3\t4\t5\t"
+print(tabbedString)
+print('The pet shop owner said "No, no \'e\'s uh,... he\'s resting"')
+print("The pet shop owner said \"No, no 'e's uh... he's resting\"")
+anotherSplitString = """This string has been 
+split over
+several lines"""
+print(anotherSplitString)
+
+print('''The pet shop owner said "No, no, 'e's' uh... he's resting" ''')
+print(""" The pet shop owner said "No, no, 'e's' uh... he's resting" """)
