cb8b165a833bdd6b10c6d1990d18c2acfade25eb
Abhijeet Vaikar <vaikar.abhijeet@gmail.com>
2017-10-08 21:31:05 +0530

    Learnt how to work with variables

---
+++ variables.py
@@ -0,0 +1,56 @@
+__author__ = 'dev'
+greeting = "Bruce"
+_myName = "Abhijeet"
+Tim45 = "Good"
+Tim_Was_57 = "Hello"
+Greeting = "There"
+
+age = 24
+print(age)
+
+'''print (greeting + age)'''
+
+a = 12
+b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+print(a + b / 3 - 4 * 12)
+print(8 / 2 * 3)
+print(8 * 3 / 2)
+
+print((((a+b)/3)-4)*12)
+
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
+
+parrot = "Norwegian Blue"
+print(parrot)
+print(parrot[0])
+print(parrot[-1])
+print(parrot[-2])
+print(parrot[0:5])
+print(parrot[:6])
+print(parrot[6:])
+print(parrot[-4:-2])
+print(parrot[0:6:3])
+
+number = "9,223,372,036,854,775,807"
+print(number[1::4])
+
+numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+print(numbers[0::3])
+print("Hello"*5)
+print("Hello" * (5+4))
+print("Hello" * 5 + "4")
+today = "Friday"
+print("day" in today)
+print("Fri" in today)
+print("Thur" in today)
+print("parrot" in "fjord")
